c954df6be0ceb1c0cb172e1b938004ff73e0ba51
Prakriti <prakritimaiti98@gmail.com>
2021-11-18 18:18:57 +0530

    17 & 18 file completed

--- Day_3/Pattern18.cpp
+++ Day_3/Pattern18.cpp
@@ -16,11 +16,28 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int row=1;row<=2*n;row++){
+    //for 1st half
+    for(int row=1;row<=n;row++){
         for(int col=n;col>=row;col--){
             cout<<"*";
         }
         for(int col=1;col<row;col++){
+            cout<<"  ";
+        }
+        for(int col=n;col>=row;col--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //for 2nd half
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            cout<<"*";
+        }
+        for(int col=n;col>row;col--){
+            cout<<"  ";
+        }
+        for(int col=1;col<=row;col++){
             cout<<"*";
         }
         cout<<endl;
