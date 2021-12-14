63ca0321854e628380f1bffc5b1065e85f7bc6d7
Prakriti <prakritimaiti98@gmail.com>
2021-12-14 22:52:10 +0530

    bitwise

---
+++ Day_30/Trailing_Zeroes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    while(n){
+        n=n>>1;
+        count++;
+        
+    }
+    cout<<count;
+    return 0;
+}
